04cdbf0be148684731bbb81fdd02be7427200449
unknown <olysiuk01@gmail.com>
2020-09-30 17:46:36 +0300

    toolbar/drawer layout

---
+++ src/components/toolbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-card height="85px" flat tile>
+    <v-toolbar class="deep-purple darken-2" dark>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Ideas App</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon class="mr-10">
+        Login
+      </v-btn>
+    </v-toolbar>
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      class="deep-purple darken-2"
+      temporary
+      overlay-opacity="0.7"
+    >
+    </v-navigation-drawer>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "toolbar",
+  data: () => {
+    return {
+      drawer: false
+    };
+  }
+};
+</script>
