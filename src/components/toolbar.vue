17b950b7332bd079525a7b5b02e741f81f1bb715
unknown <olysiuk01@gmail.com>
2020-09-30 21:32:38 +0300

    registration view layout

--- src/components/toolbar.vue
+++ src/components/toolbar.vue
@@ -19,8 +19,26 @@
       app
       class="deep-purple darken-2"
       temporary
+      dark
       overlay-opacity="0.7"
     >
+      <v-list>
+        <v-list-item-group v-model="elem">
+          <v-list-item
+            v-for="(item, index) in items"
+            :key="index"
+            router
+            :to="item.route"
+          >
+            <v-list-item-icon>
+              <v-icon v-text="item.icon" color="orange lighten-3"></v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.text }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
     </v-navigation-drawer>
   </v-card>
 </template>
@@ -28,9 +46,14 @@
 <script>
 export default {
   name: "toolbar",
-  data: () => {
+  data() {
     return {
-      drawer: false
+      drawer: false,
+      elem: 1,
+      items: [
+        { text: "Home", icon: "mdi-home", route: "/" },
+        { text: "About", icon: "mdi-information", route: "/about" }
+      ]
     };
   }
 };
--- src/views/Registration.vue
+++ src/views/Registration.vue
@@ -2,7 +2,7 @@
   <v-container>
     <v-card max-width="550px" class="mx-auto">
       <v-card-title class="justify-center mt-2 mb-3" style="font-size:35px">
-        Log in
+        Registration
       </v-card-title>
       <v-form>
         <v-row justify="center">
@@ -12,21 +12,19 @@
           <v-col cols="12" sm="8" class="pt-2 pb-0">
             <v-text-field label="Password" v-model="password"></v-text-field>
           </v-col>
-          <v-col cols="12" sm="8" class="mt-2 text-center">
-            <v-btn class="deep-purple darken-2" dark width="130" type="submit"
-              >Log in</v-btn
-            >
+          <v-col cols="12" sm="8" class="pt-2 pb-0">
+            <v-text-field
+              label="Password again"
+              v-model="repeatPassword"
+            ></v-text-field>
           </v-col>
-          <v-col cols="12" sm="8" class="mb-5 pt-0 text-center">
-            <div>
-              New to ideas app?
-              <router-link
-                class="text-decoration-none"
-                router
-                to="/registration"
-                >Get started</router-link
-              >
-            </div>
+          <v-col cols="12" sm="8" class="mt-2 mb-0 pt-0 pb-0 text-center">
+            <v-checkbox label="Agree" v-model="agree" />
+          </v-col>
+          <v-col cols="12" sm="8" class="mt-2 mb-5 text-center">
+            <v-btn class="deep-purple darken-2" dark width="130" type="submit"
+              >Registration</v-btn
+            >
           </v-col>
         </v-row>
       </v-form>
@@ -36,12 +34,16 @@
 
 <script>
 export default {
-  name: "Login",
+  name: "Registration",
   data() {
     return {
       email: "",
-      password: ""
+      password: "",
+      repeatPassword: "",
+      agree: false
     };
   }
 };
 </script>
+
+<style></style>
